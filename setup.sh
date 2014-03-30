f6d79fea5535b3a3b790791503245d04045f9276
kimat <skimat@gmail.com>
2014-03-31 00:33:34 +0200

    1. added bashrc 2. [setup.sh] proper dir symlinking

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/bash_aliases ~/.bash_aliases
 ln -s ~/dotfiles/dircolors ~/.dircolors
-ln -s ~/dotfiles/config/htop ~/.config/htop
+ln -s ~/dotfiles/bashrc ~/.bashrc
+ln -s ~/dotfiles/config/htop -t /root/.config
