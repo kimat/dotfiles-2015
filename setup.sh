28fae3d3779d315226fd1e139960fb9f33792171
kimat <skimat@gmail.com>
2014-03-31 00:12:17 +0200

    added dircolors : for messed up colors on samba , mounted NTFS shares etc

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@
 # make symlinks to these dotfiles
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/bash_aliases ~/.bash_aliases
+ln -s ~/dotfiles/dircolors ~/.dircolors
 ln -s ~/dotfiles/config/htop ~/.config/htop
