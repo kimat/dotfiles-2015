e9219fc7ea78128553501282d77156f355da17dd
kimat <skimat@gmail.com>
2014-04-01 15:55:03 +0200

    added tmux.conf

--- setup.sh
+++ setup.sh
@@ -4,4 +4,7 @@ ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/bash_aliases ~/.bash_aliases
 ln -s ~/dotfiles/dircolors ~/.dircolors
 ln -s ~/dotfiles/bashrc ~/.bashrc
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+
+
 ln -s ~/dotfiles/config/htop -t /root/.config
