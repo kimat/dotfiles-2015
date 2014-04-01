9dcf894b3aaa600490dc1d863b3901f13d51d05f
kimat <skimat@gmail.com>
2014-04-01 23:08:33 +0200

    added gitignore

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ ln -s ~/dotfiles/bash_aliases ~/.bash_aliases
 ln -s ~/dotfiles/dircolors ~/.dircolors
 ln -s ~/dotfiles/bashrc ~/.bashrc
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/gitignore ~/.gitignore
 
 
 ln -s ~/dotfiles/config/htop -t /root/.config
