04d6e86af726de749e64915c299f1ac536fe7fab
kimat <skimat@gmail.com>
2014-05-19 14:56:12 +0200

    [setup] added bin folder & tmuxifier

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # make symlinks to these dotfiles
 ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/tmuxifier ~/.tmuxifier
+ln -s ~/dotfiles/tmuxifier/bin/tmuxifier ~/dotfiles/bin/tmuxifier
 ln -s ~/dotfiles/bash_aliases ~/.bash_aliases
 ln -s ~/dotfiles/dircolors ~/.dircolors
 ln -s ~/dotfiles/bashrc ~/.bashrc
@@ -8,7 +10,7 @@ ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/gitignore ~/.gitignore
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 
-
-
 ln -s ~/dotfiles/vim -T /root/.vim
 ln -s ~/dotfiles/config/htop -t /root/.config
+
+ln -s ~/dotfiles/bin /usr/local/bin
