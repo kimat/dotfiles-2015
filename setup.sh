450ea121e50d3a33ab09e4c02ab09d6dd3fbdaf0
kimat <skimat@gmail.com>
2014-06-06 14:40:29 +0200

    [setup] still moving to github

--- setup.sh
+++ setup.sh
@@ -19,3 +19,6 @@ ln -s ~/dotfiles/gitignore ~/.gitignore
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/config/htop -t /root/.config
 
+# If no such dir : ~/.vim/bundle
+mkdir -p ~/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
