2e379fc606261263283df5aa23490bcd78d122ed
kimat <skimat@gmail.com>
2014-06-06 09:55:06 +0200

    [tmux] changed selected window color & simplified main layouts

--- tmuxifier/layouts/main.session.sh
+++ tmuxifier/layouts/main.session.sh
@@ -5,37 +5,40 @@ session_root "/root/"
 # Create session with specified name if it does not already exist. If no
 # argument is given, session name will be based on layout file name.
 if initialize_session "main"; then
-  new_window "ecam"
+  new_window "markdown"
   #----------------
-  run_cmd "cd /vagrant/ecam/"
-  run_cmd "ve"
-  split_h
-  select_pane 1
-  run_cmd "cd /vagrant/ecam/"
-  split_v
-  select_pane 2
-  run_cmd "cd /vagrant/ecam/"
-  
+  run_cmd "cd /vagrant/"
+  run_cmd "clear"
+  run_cmd "vm"
 
-  new_window "vim|tmux"
-  #--------------------
-  run_cmd "cd /root/dotfiles/"
-  run_cmd "vv"
-  split_h
-  select_pane 1
-  run_cmd "cd /root/dotfiles/"
-  run_cmd "vt"
-
-  new_window "git"
+  new_window "dotfiles"
   #---------------
   run_cmd "cd /root/dotfiles/"
-  split_h
+  run_cmd "clear"
+  run_cmd "vd"
+  split_h 40
   select_pane 1
   run_cmd "cd /root/dotfiles/"
+  run_cmd "clear"
   split_v 30
   select_pane 2
   run_cmd "cd /root/dotfiles/"
+  run_cmd "clear"
   run_cmd "gs"
+  select_pane 0
+
+  new_window "launcher"
+  #---------------
+  run_cmd "cd /root/launcher/"
+  run_cmd "clear"
+  split_h
+  select_pane 1
+  run_cmd "cd /root/launcher/"
+  run_cmd "clear"
+  split_v 30
+  select_pane 2
+  run_cmd "cd /root/launcher/"
+  run_cmd "clear"
 
   new_window "view"
 
