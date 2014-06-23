65ba80103980a0f06881f77ff0867ffca485f6c0
kimat <vagrant@vagrantbox.ecam.be>
2014-06-23 13:09:44 +0200

    [setup] removed /root refs & fixed bin path

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-
 # make symlinks to these folders
 ln -s ~/dotfiles/vim -T ~/.vim
-ln -s ~/dotfiles/bak ~/.bak
 ln -s ~/dotfiles/fzf ~/.fzf
 ln -s ~/dotfiles/bin ~/bin
 
@@ -12,19 +10,22 @@ ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/fzf.bash ~/.fzf.bash
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/tmuxifier ~/.tmuxifier
-ln -s ~/dotfiles/tmuxifier/bin/tmuxifier ~/dotfiles/bin/tmuxifier
 ln -s ~/dotfiles/bash_aliases ~/.bash_aliases
 ln -s ~/dotfiles/dircolors ~/.dircolors
 ln -s ~/dotfiles/bashrc ~/.bashrc
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/gitignore ~/.gitignore
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/dotfiles/config/htop -t /root/.config
+ln -s ~/dotfiles/config/htop -t ~/.config
 
 # Vim
 # Vim.NeoBundle : If no such dir : ~/.vim/bundle
-mkdir -p ~/.vim/bundle
-git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+if ! [ -d "~/.vim/bundle" ]; then
+  mkdir -p ~/.vim/bundle
+  git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+fi
+
 # Vim.ctrl-space
 ln -s ~/dotfiles/cs_workspaces ~/dotfiles/.git/cs_workspaces
 
+exec bash -l
